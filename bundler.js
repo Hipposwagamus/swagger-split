ed5e3a079e76c445fc0d73e7a0c02739664c222d
Aaron Harrington <aharrington@aviatainc.com>
2016-12-20 17:28:36 -0700

    first commit

---
+++ bundler.js
@@ -0,0 +1,57 @@
+const YAML = require('js-yaml');
+const fs = require('fs');
+var path = require('path');
+var _ = require('lodash');
+
+const defaultMaxDepth = 10;
+
+function loadFile(file){
+  let type = path.extname(file);
+  if(type === '.json'){
+    return JSON.parse(fs.readFileSync(file).tostring());
+  } else if (type === '.yaml'){
+    return YAML.load(fs.readFileSync(file).toString());
+  }
+}
+
+function constructSwagger(obj, dir, depthLeft){
+  if(depthLeft < 0){
+    throw new Error(`Maximum reference depth exceeded (Default is ${defaultMaxDepth})`);
+  }
+  if(!_.isNil(obj.$ref) && !_.startsWith(obj.$ref, '#')){
+    let nextFile = path.resolve(dir, obj.$ref);
+    return constructSwagger(loadFile(nextFile), path.dirname(nextFile), depthLeft - 1);
+  } else {
+    return _.mapValues(obj, (v) => {
+      if(_.isObject(v) && !_.isArray(v)){
+        return constructSwagger(v, dir, depthLeft - 1);
+      } else {
+        return v;
+      }
+    });
+  }
+}
+
+function bundle(indexFile, options){
+  const maxDepth = (_.isNil(options.maxDepth) ? defaultMaxDepth : options.maxDepth ); 
+  const initialDir = path.dirname(indexFile);
+
+  const obj = constructSwagger(loadFile(indexFile), initialDir, maxDepth);
+
+  if(!_.isNil(options.outFile)){
+    let type = path.extname(outFile);
+    if(type === ".json") {
+      fs.writeFileSync(options.outFile, JSON.stringify(obj));
+    } else if (type === ".yaml") {
+      fs.writeFileSync(options.outFile, YAML.dump(obj));
+    } else {
+      throw new Error(`The provided output format of ${type} is not supported`);
+    }
+  } else {
+    return obj;
+  }
+}
+
+module.exports = {
+  bundle
+};
---
+++ index.js
@@ -0,0 +1,64 @@
+const args = require('minimist')(process.argv.slice(2));
+const fs = require('fs');
+const yaml = require('js-yaml');
+const _ = require('lodash');
+const path = require('path');
+const camelCase = require('camelcase');
+
+if(args._.length === 3){
+  const yamlFile = args._[0];
+  const depth = args._[1];
+  const outputDir = args._[2];
+  try {
+    var doc = yaml.safeLoad(fs.readFileSync(yamlFile, 'utf8'));
+    fs.mkdirSync(outputDir);
+    split(doc, depth, outputDir);
+  } catch (e) {
+    console.error(e);
+  }
+} else {
+  console.error("not enough args. Must be node index.js <file-to-read> <depth> <output-dir>");
+}
+
+function directify(name){
+  return camelCase(name.replace(/[^\w\s]/gi, '_'));
+}
+
+function split(obj, depth, dir){
+  if (depth === 0){
+    let indexObj = {};
+    _.forIn(obj, (v, k) => {
+      if(_.isObject(v) && !_.isArray(v)){
+        let kFile = directify(k) + ".yaml";
+        let file = path.join(dir, kFile);
+        indexObj[k] = {"$ref": './' + kFile}
+        fs.writeFileSync(file, yaml.dump(v));
+      } else {
+        indexObj[k] = v;
+      }
+
+      fs.writeFileSync(path.join(dir, "index.yaml"), yaml.dump(indexObj));
+    });
+  } else {
+    let indexObj = {};
+    _.forIn(obj, (v, k) => {
+      if(_.isObject(v) && !_.isArray(v)){
+        let newDir = path.join(dir, directify(k));
+        indexObj[k] = {"$ref": './' + path.join(directify(k), "index.yaml")}
+        fs.mkdirSync(newDir);
+        split(v, depth - 1, newDir);
+      } else {
+        indexObj[k] = v;
+      }
+    });
+
+    fs.writeFileSync(path.join(dir, "index.yaml"), yaml.dump(indexObj));
+
+  }
+
+  function splitFile(file, depth, outputDirectory){
+    var doc = yaml.safeLoad(fs.readFileSync(file, 'utf8'));
+    fs.mkdirSync(outputDirectory);
+    split(doc, depth, outputDirectory);
+  }
+}
